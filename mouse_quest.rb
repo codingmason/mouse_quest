2855a8bc1443c69d7a0506b46768b914b79dc394
Mason Brown <mason.patrick.brown@gmail.com>
2016-06-04 03:35:43 -0400

    Pseudocode more methods and some outlines for databases

--- mouse_quest.rb
+++ mouse_quest.rb
@@ -4,15 +4,27 @@
 
 *** Character Class ***
 
-
-
+	-Character name
+	-Character Spellbook is an array of spell names
+	-Location equals an array containing an X and Y axis value
+	-Money equals the treasure they've accrued
 
 
 
 ========= DATABASES =========
 
+*** Saved Character Database ***
 
 
+
+
+
+*** Spellbook Database ***
+
+	-Primary Key (should match the Primary Key of the Saved Character Database)
+	-Name (from the Character Name)
+	-Columns for each spell with boolean values showing whether the character knows
+	 the spell or not
 
 
 ========= METHODS =========
@@ -23,26 +35,81 @@
 	-UNTIL correct input is TRUE
 		-Ask if the user would like to load a character or create a character
 			-IF Load, run Load Character Method. Correct input is TRUE
-			-IF Create, run Create Character Method. Correct input is TRUE
-			-IF ELSE, PUTS an 'I didn't understand that' message. Correct input is FALSE
+			-ELSIF Create, run Create Character Method. Correct input is TRUE
+			-ELSE, PUTS an 'I didn't understand that' message. Correct input is FALSE
 
 *** Create Character Method ***
 
 	-PUTS a question asking for a character name
 	-GETS the response. Assign as a variable 'character name'
-	-PUTS a question asking if they would prefer a rapier and buckler or a greatsword
-	-GETS the response. Assign as a variable 'current weapon'
-	-CREATE an instance of the Character Class, assigning the character name and current weapon
+	-UNTIL correct input is TRUE
+		-PUTS a question asking to choose between a few different spells they could use
+		-GETS the response. 
+			-IF response is correct, assign response as variable 'spell', input is TRUE
+			-ELSE PUTS something like 'I really think you should arm yourself with knowledge'
+	-CREATE an instance of the Character Class called Current Character
+	-Assign character name and spell to Current Character class variables
 	-Query SQLite to to add Character information to the Saved Characters database
+	-Query SQLite to to add spell information to the Spellbook database
 
 *** Load Character Method ***
 
 	-Query SQLite to PUTS names of saved characters and their primary keys 
-	-Ask the player to choose 
+	-UNTIL correct input is TRUE
+		-PUTS a question ask the player to pick the character number
+		-GETS the response. 
+			-IF response is correct, CREATE an instance of the Character Class 
+			 called Current Character using info queried from Saved Character 
+			 and Saved Inventory databases
+			-ELSE PUTS 'That character does not exist'
+	-PUTS a summary of the character, spellbook, and inventory.
+
+
+*** Cheesewright Inn Method ***
+	
+	-UNTIL correct input is TRUE
+		-PUTS a description of the inn. The proprieter, Sam Butterwhiskers, asks if
+	 	they would like to rest or venture forth
+		-GETS response
+			-IF rest, run the Save Method. Correct input is TRUE
+			-ELSIF venture forth, run the Move Method. Correct input is TRUE
+			-ELSE, PUTS an 'I didn't understand that' message. Correct input is FALSE
+
+*** Save Method ***
+
+	-Query SQLite and push Current Character attributes to Saved Character Database 
+	 with matching name
+	-Query SQLite and push Character Spellbook attributes to Spellbook Database with 
+	 matching name
+	-PUTS question if they would like to continue or not
+		-IF yes, PUTS description of waking up at the inn and have Sam wish them good
+		luck on their quest. Run the Move Method 
+		-ELSE, PUTS a goodbye message and end program
+
+
+*** Move Method ***
+	-UNTIL correct input is TRUE
+		-PUTS would you like to move North, East, South, or West
+		-GETS answer
+			-IF North add 1 to Current Character's Location y axis. Correct input TRUE 
+			-ELSIF East add 1 to Current Character's Location x axis. Correct input TRUE  	
+			-ELSIF South add -1 to Current Character's Location y axis. Correct input TRUE  	
+			-ELSIF East add -1 to Current Character's Location x axis. Correct input TRUE  	
+			-ELSE PUTS that is not a valid direction. . Correct input FALSE 
+		-PUTS 'You venture' the chosen direction 'and soon come upon...'
+		-Run the New Location Method
+
+*** New Location Method ***
 
 
 
 
+
+
+
+========= Hashes =========
+
+*** Spell Hash ***
 
 
 	
